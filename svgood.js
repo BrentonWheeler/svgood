b49f4fe6bf67098ece602af695e175b7697abffc
Brenton Wheeler <brenton.wheeler@rexsoftware.com.au>
2018-07-14 23:57:25 +1000

    Init commit, it works

---
+++ svgood.js
@@ -0,0 +1,58 @@
+var fs = require('fs');
+var dir = process.env.NEW_DIR_NAME || './new-icons';
+
+function tryMakeDirRecursively(attempt){
+	const tempDir = dir + (attempt ? '-' + attempt : '');
+	if (!fs.existsSync(tempDir)){
+		dir = tempDir;
+		fs.mkdirSync(tempDir);
+	} else {
+		tryMakeDirRecursively(attempt + 1)
+	}
+}
+
+function createBasicSvg(fileName, content){
+	// Regex
+	const shapeRegex = /<.*id="path-1".*>/
+	const defsRegex = /<defs((.|\n)*)<\/defs>/
+	const gRegex = /<g((.|\n)*)<\/g>\n/
+	const path = content.match(shapeRegex)[0]
+	const defs = content.match(defsRegex)[0]
+	const g = content.match(gRegex)[0]
+
+	// Removing unwanted content
+	content = content.replace(g, '');
+	content = content.replace(defs, path);
+
+	// Creating a new svg with resulting content
+	fs.appendFile('./' + dir + '/' + fileName, content, function(err){
+		if(err){throw(err)}
+	});
+}
+
+function readFiles(dirname, onFileContent, onError) {
+  fs.readdir(dirname, function(err, filenames) {
+    if (err) {
+      onError(err);
+      return;
+    }
+    filenames.forEach(function(filename) {
+		if(filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2) === 'svg'){
+			fs.readFile(dirname + filename, 'utf-8', function(err, content) {
+				if (err) {
+				onError(err);
+				return;
+				}
+				onFileContent(filename, content);
+			});
+		}
+	});
+  });
+}
+
+tryMakeDirRecursively(0);
+if(process.env.ICON_SOURCE){
+	readFiles(process.env.ICON_SOURCE, createBasicSvg, console.log);
+} else {
+	throw new Error("Please specify an ICON_SOURCE directory location from which to source .svg's")
+}
